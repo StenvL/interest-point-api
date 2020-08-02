7b3508a4661aab93a6cd31fa6dc6d03c421c5eff
Valeriy <sten.vl2010@gmail.com>
2020-08-02 10:02:29 +0500

    Fix points seed city id mistakes

--- db/migrations/dev/20200801175343_seed_points.up.sql
+++ db/migrations/dev/20200801175343_seed_points.up.sql
@@ -4,5 +4,5 @@ VALUES
     ("Opera and Ballet Theatre", "Famous opera and ballet theatre in Perm, founded in 1879.", 2, 2, 58.015627, 56.246195),
     ("PermM", "Modern art museum", 1, 2, 58.012586, 56.279213),
     ("Bolshoi Theatre", "One of the most famous in Russia and in the world opera and ballet theatre, founded in 1776.", 2, 1, 55.760318, 37.619191),
-    ("Tretyakov Gallery", "Famous art gallery in Moscow, founded in 1856 by merchant Pavel Tretyakov.", 2, 2, 55.741562, 37.621046),
-    ("Luzhniki stadium", "The biggest stadium in Russia, accomodates about 81000 spectators.", 4, 2, 55.717030, 37.556214);
+    ("Tretyakov Gallery", "Famous art gallery in Moscow, founded in 1856 by merchant Pavel Tretyakov.", 1, 1, 55.741562, 37.621046),
+    ("Luzhniki stadium", "The biggest stadium in Russia, accomodates about 81000 spectators.", 4, 1, 55.717030, 37.556214);
